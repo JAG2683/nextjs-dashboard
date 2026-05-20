88f9a96cf0554fd965e057b5d9fbc39af346c198
Jose Gonzalez <86135928+JAG2683@users.noreply.github.com>
2026-05-20 19:54:40 -0400

    Finished dashboard project

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,53 @@
-export default function Page() {
-  return <p>Customers Page</p>;
+import { Metadata } from 'next';
+
+import Search from '@/app/ui/search';
+
+import Table from '@/app/ui/customers/table';
+
+import { InvoicesTableSkeleton }
+  from '@/app/ui/skeletons';
+
+import { Suspense } from 'react';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default async function Page(
+  props: {
+    searchParams?: Promise<{
+      query?: string;
+      page?: string;
+    }>;
+  },
+) {
+  const searchParams =
+    await props.searchParams;
+
+  const query =
+    searchParams?.query || '';
+
+  return (
+    <div className="w-full">
+
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl">
+          Customers
+        </h1>
+      </div>
+
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search customers..." />
+      </div>
+
+      <Suspense
+        key={query}
+        fallback={
+          <InvoicesTableSkeleton />
+        }
+      >
+        <Table query={query} />
+      </Suspense>
+    </div>
+  );
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,6 +7,12 @@ import LoginForm
 import { Suspense }
   from 'react';
 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Login',
+};
+
 export default function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
